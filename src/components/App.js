b50706c1275b69d92d6faaa3e5e2ab084d1cdbbe
Nolan Rummel <nolan.rummel22@gmail.com>
2023-06-21 21:08:10 -0400

    a few deliverables completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -7,9 +7,17 @@ import ToyContainer from "./ToyContainer";
 function App() {
   const [showForm, setShowForm] = useState(false);
 
+  const [toys, setToys] = useState([])
+
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
+
+  useEffect(() =>{
+    fetch('http://localhost:3001/toys')
+    .then(r => r.json())
+    .then(toysData => setToys(toysData))
+  }, [])
 
   return (
     <>
@@ -18,7 +26,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys}/>
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ToyForm() {
+
+  const blankToy = {
+    id: "",
+    name: "",
+    image: "",
+    likes: "0"
+  }
+
+  const [formData, setFormData] = useState(blankToy)
+
+  const handleData = e => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  console.log('formData: ', formData)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    //POST formData
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
-        <input
+        <input onChange={handleData}
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
         />
         <br />
-        <input
+        <input onChange={handleData}
           type="text"
           name="image"
           placeholder="Enter a toy's image URL..."
